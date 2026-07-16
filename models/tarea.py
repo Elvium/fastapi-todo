b201a4e047af6c8f2fc7c12e13603a761ae5e4c2
Elvium <oscar.ronaldo.96@gmail.com>
2026-07-15 23:17:46 -0500

    Add sqlalchemy and introduction

---
+++ models/tarea.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Tarea(Base):
+
+    __tablename__ = "tareas"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True
+    )
+
+    descripcion: Mapped[str] = mapped_column(
+        String(100)
+    )
+
+    prioridad: Mapped[int] = mapped_column(
+        Integer
+    )
+
+    estado: Mapped[str] = mapped_column(
+        String(30)
+    )
